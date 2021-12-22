eda02e98c007581284f9277ecf80031811f4d6e0
t108360145 <t108360145@ntut.org.tw>
2021-12-22 21:34:30 +0800

    first

---
+++ pa/src/pa/test_p8.java
@@ -0,0 +1,5 @@
+package pa;
+
+public class test_p8 {
+
+}
---
+++ pb/src/pb/test_p10.java
@@ -0,0 +1,11 @@
+package pb;
+import pc.Car;
+public class test_p10 {
+
+	public static void main(String[] args) {
+
+		Car car1 = new Car(); 
+		car1.show();
+	}
+
+}
